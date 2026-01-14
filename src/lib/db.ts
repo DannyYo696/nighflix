23fafcd2a66db5f866f282816a7c46037eab3ec6
Daniel Fatunse <66962552+DannyYo696@users.noreply.github.com>
2026-01-14 10:10:39 +0100

    Refactor db.ts to initialize PrismaClient directly

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
-
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-  })
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+export const db = new PrismaClient({
+  adapter: process.env.DATABASE_URL,
+  log: ['query'],
+})
